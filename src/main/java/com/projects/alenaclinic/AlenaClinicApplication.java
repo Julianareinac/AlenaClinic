9b1a87bed3e311ca936b655ce479332efe105912
Juliana <julianareina.c@hotmail.com>
2024-06-05 21:59:28 -0500

    first commit

---
+++ src/main/java/com/projects/alenaclinic/AlenaClinicApplication.java
@@ -0,0 +1,13 @@
+package com.projects.alenaclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlenaClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlenaClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projects/alenaclinic/AlenaClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.alenaclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlenaClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
